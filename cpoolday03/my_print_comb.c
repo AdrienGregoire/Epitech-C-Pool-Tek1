f58777a6415db05ad9e5f5b5e094d000d26d5d9c
Adrien Gregoire <adrien.gregoire@epitech.eu>
2025-09-21 13:52:10 +0200

    feat(my_print_comb.c): the print comb function

---
+++ cpoolday03/my_print_comb.c
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2024
+** my_print_comb
+** File description:
+**  the numbers composed by three different digits numbers
+*/
+
+#include <stdio.h>
+void my_putchar(char c);
+
+int my_print_comb(void)
+{
+    int a = 0;
+    int b = 1;
+    int c = 2;
+    int iteration = 0;
+
+    for (; c < 9 || a < 7 || b < 8; c++) {
+        if (c > 9) {
+            b++;
+            c = b + 1;
+        }
+        if (b > 8) {
+            a++;
+            b = a + 1;
+            c = b + 1;
+        }
+    my_putchar(a + '0');
+    my_putchar(b + '0');
+    my_putchar(c + '0');
+    my_putchar(' ');
+    }
+}
